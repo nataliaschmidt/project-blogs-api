04447e4b3218b28f798514b1093f1c8299af5b37
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-07-15 15:38:37 -0300

    Fix: corrige erro de lint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const loginRoutes = require('./loginRoute');
 const userRoutes = require('./userRoute');
 const categoriesRoutes = require('./categoriesRoute');
+
 module.exports = {
   loginRoutes,
   userRoutes,
